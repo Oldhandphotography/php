5168e13538e384bc7b0791cd53dfd66cab955d24
souvik <bac_02821402019souvikbhardwaj@jimsd.org>
2021-12-08 10:02:06 +0530

    commit

--- Crud-project/crud.php
+++ Crud-project/crud.php
@@ -1,5 +1,6 @@
 <?php
 // INSERT INTO `notes` (`sno`, `title`, `description`, `tstamp`) VALUES (NULL, 'buy books ', 'please buy books from book store', current_timestamp());
+$insert=false;
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -12,10 +13,16 @@ if (!$conn) {
 }
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   $title = $_POST["title"];
-  $description = $_POST["description"];
+  $desc = $_POST["description"];
   //sql query
-  $sql = "INSERT INTO 'notes' (`title`,`description`) VALUES (`$title` , `$description`) ";
+  $sql = "INSERT INTO `notes` (`sno`, `title`, `description`, `tstamp`) VALUES (NULL, '$title', '$desc', current_timestamp())";
   $result = mysqli_query($conn, $sql);
+  if($result){
+    $insert=true;
+  }
+  else{
+    echo "the".mysqli_error($conn);
+  }
 }
 // $sql = "SELECT * FROM `information`";
 ?>
@@ -29,6 +36,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <title>Professor-Notes</title>
 </head>
 
@@ -72,9 +81,17 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
       </div>
     </div>
   </nav>
+  <?php
+  if($insert === true){
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>Hey</strong> Youre note is successfully inserted below under the table.
+    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+  </div>";
+}
+  ?>
   <div class="container">
     <h2 class="pt-4 pl-6">Add a Note</h2>
-    <form class="pt-8 p-4" action="/crud.php" method="post">
+    <form class="pt-8 p-4" action="" method="post">
       <div class="mb-3">
         <label for="title" class="form-label font-bold">Notes Title</label>
         <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
@@ -83,14 +100,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
       <h2 class="form-text font-bold p-2">Note</h2>
       <div class="form-floating">
-        <textarea class="form-control" placeholder="Leave a comment here" id="description" name="desc" style="height: 100px"></textarea>
+        <textarea class="form-control" placeholder="add description here" id="description" name="desc" style="height: 100px"></textarea>
         <label for="desc">Write Note Here</label>
       </div>
 
       <button type="submit" class = "btn btn-primary mt-2">Add Note</button>
     </form>
   </div>
-  <div class="container">
+  <div class="container" id="myTable">
     <table class="table table-striped">
       <thead>
         <tr>
@@ -119,6 +136,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+     } );
+  </script>
 </body>
 
 </html>
